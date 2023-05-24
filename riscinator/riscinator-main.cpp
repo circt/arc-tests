b59dcc04d57c60c666bd3ee482e14fa0892bf587
Martin Erhart <maerhart@outlook.com>
2023-05-24 22:12:24 +0200

    Add dhrystone benchmark for riscinator

--- riscinator/riscinator-main.cpp
+++ riscinator/riscinator-main.cpp
@@ -1,4 +1,5 @@
 #include "riscinator.h"
+#include <cstdio>
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
@@ -6,9 +7,11 @@
 #include <cassert>
 #include <cstring>
 #include <vector>
+#include <chrono>
 
 #include "itype_mem.h"
 #include "jmps_mem.h"
+#include "dhrystone_mem.h"
 
 typedef struct {
     uint32_t idx;
@@ -35,8 +38,10 @@ static unsigned addr2idx(uint32_t addr, size_t mem_base) {
 }
 
 static bool failed = false;
+static bool finished = false;
+static bool verbose = false;
 
-static void simulate(Core &model, ValueChangeDump<CoreLayout> vcd, uint32_t* mem, size_t len, size_t mem_base, check_t* check) {
+static void simulate(Core &model, ValueChangeDump<CoreLayout> vcd, uint32_t* mem, size_t len, size_t mem_base, check_t* check, unsigned max_cycles) {
     auto &core = model.view;
     core.reset = 1;
     model.passthrough();
@@ -45,15 +50,15 @@ static void simulate(Core &model, ValueChangeDump<CoreLayout> vcd, uint32_t* mem
     model.passthrough();
     vcd.writeTimestep(1);
     core.reset = 0;
+    model.passthrough();
 
     uint32_t next_imem_rdata = 0;
     uint8_t next_imem_rvalid = 0;
     uint32_t next_dmem_rdata = 0;
     uint8_t next_dmem_rvalid = 0;
 
-    unsigned ncyc = 50;
-    for (unsigned i = 0; i < ncyc; i++) {
-        model.passthrough();
+    auto duration = std::chrono::high_resolution_clock::duration::zero();
+    for (unsigned i = 0; i < max_cycles; i++) {
         vcd.writeTimestep(1);
 
         core.io_imem_rvalid = next_imem_rvalid;
@@ -64,25 +69,70 @@ static void simulate(Core &model, ValueChangeDump<CoreLayout> vcd, uint32_t* mem
         next_imem_rvalid = core.io_imem_req;
         next_dmem_rvalid = core.io_dmem_req;
         core.io_dmem_gnt = core.io_dmem_req;
+
+        auto t0 = std::chrono::high_resolution_clock::now();
         model.passthrough();
+        auto t1 = std::chrono::high_resolution_clock::now();
+        duration += t1 - t0;
 
         if (core.io_imem_req) {
-            assert(addr2idx(core.io_imem_addr, mem_base) < len);
-            next_imem_rdata = mem[addr2idx(core.io_imem_addr, mem_base)];
+            if (verbose) {
+                printf("reading instruction at 0x%x: ", core.io_imem_addr);
+                // Flush to get it out before the assert below.
+                fflush(stdout);
+            }
+            auto memidx = addr2idx(core.io_imem_addr, mem_base);
+            assert(memidx < len);
+            next_imem_rdata = mem[memidx];
+            if (verbose) printf("0x%x\n", next_imem_rdata);
         }
 
         if (core.io_dmem_req && core.io_dmem_we) {
             uint32_t write = core.io_dmem_wdata;
             uint32_t mask = be2mask(core.io_dmem_be);
-            assert(addr2idx(core.io_dmem_addr, mem_base) < len);
-            mem[addr2idx(core.io_dmem_addr, mem_base)] = write & mask;
+            auto memidx = addr2idx(core.io_dmem_addr, mem_base);
+            if (verbose) {
+                printf("writing 0x%x to 0x%x (with mask 0x%x)\n", write & mask, core.io_dmem_addr, mask);
+                // Flush to get it out before the assert below.
+                fflush(stdout);
+            }
+            if (core.io_dmem_addr == 0xffff00) {
+                finished = true;
+                fprintf(stderr, "** dhrystones per second: %u **\n", write & mask);
+                fprintf(stderr, "** execution finished at cycle %u **\n", i);
+                break;
+            } else {
+                assert(memidx < len);
+                mem[memidx] = (write & mask) | (~mask & mem[memidx]);
+            }
         } else if (core.io_dmem_req) {
-            assert(addr2idx(core.io_dmem_addr, mem_base) < len);
-            next_dmem_rdata = mem[addr2idx(core.io_dmem_addr, mem_base)];
+            auto memidx = addr2idx(core.io_dmem_addr, mem_base);
+            if (verbose) {
+                printf("reading data at 0x%x: ", core.io_dmem_addr);
+                // Flush to get it out before the assert below.
+                fflush(stdout);
+            }
+            assert(memidx < len);
+            next_dmem_rdata = mem[memidx];
+            if (verbose) printf("0x%x\n", next_dmem_rdata);
         }
 
+        t0 = std::chrono::high_resolution_clock::now();
         model.clock();
+        model.passthrough();
+        t1 = std::chrono::high_resolution_clock::now();
+        duration += t1 - t0;
+
+        if (i % 10000 == 0 && i != 0) {
+        auto seconds =
+            std::chrono::duration_cast<std::chrono::duration<double>>(duration)
+                .count();
+        std::cerr << "Cycle " << i << " [" << (i / seconds) << " Hz]\n";
+        }
     }
+
+    if (check == nullptr)
+      return;
 
     for (auto v : check->regs) {
         auto got = core.internal.rf.regs_ext.words[v.idx].data;
@@ -102,7 +152,7 @@ static void simulate(Core &model, ValueChangeDump<CoreLayout> vcd, uint32_t* mem
     }
 }
 
-#define MEMSIZE 4096
+#define MEMSIZE 8192*8
 #define MEMBASE 0x100000
 static uint32_t mem[MEMSIZE];
 
@@ -131,7 +181,7 @@ static void itype() {
         {0x100004, 63},
         {0x100008, 67},
     };
-    simulate(core, vcd, (uint32_t*) itype_bin, MEMSIZE, MEMBASE, check);
+    simulate(core, vcd, (uint32_t*) itype_bin, MEMSIZE, MEMBASE, check, 50);
 }
 
 static void jmps() {
@@ -168,14 +218,40 @@ static void jmps() {
         {0x100004, 63},
         {0x100008, 67},
     };
-    simulate(core, vcd, (uint32_t*) jmps_bin, MEMSIZE, MEMBASE, check);
+    simulate(core, vcd, (uint32_t*) jmps_bin, MEMSIZE, MEMBASE, check, 50);
+}
+
+static void dhrystone() {
+    Core core;
+    std::ofstream os("dhrystone.vcd");
+    auto vcd = core.vcd(os);
+
+    memset(mem, 0, sizeof(mem));
+    memcpy(mem, dhrystone_bin, dhrystone_bin_len);
+
+    simulate(core, vcd, (uint32_t*) dhrystone_bin, MEMSIZE, MEMBASE, nullptr, 10000000);
 }
 
 int main(int argc, char **argv) {
-    itype();
-    jmps();
+    if (argc > 2) {
+        fprintf(stderr, "Format: riscinator-main (itype|jmps|dhrystone)?\n");
+        return 0;
+    }
+    if (argc == 1 || !std::strcmp(argv[1], "itype")) {
+        fprintf(stderr, "** start simulating itype **\n");
+        itype();
+    }
+    if (argc == 1 || !std::strcmp(argv[1], "jmps")) {
+        fprintf(stderr, "** start simulating jmps **\n");
+        jmps();
+    }
+    if (argc == 1 || !std::strcmp(argv[1], "dhrystone")) {
+        fprintf(stderr, "** start simulating dhrystone **\n");
+        dhrystone();
+        failed |= !finished;
+    }
 
     if (failed) return 1;
-    printf("ALL TESTS PASSED\n");
+    fprintf(stderr, "ALL TESTS PASSED\n");
     return 0;
 }
